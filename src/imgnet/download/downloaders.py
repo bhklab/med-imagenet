f9298d8fb596440fcce7f2fe1470772ec8659a6d
DeclanKorda <dkorda@protonmail.com>
2026-08-10 13:23:49 -0400

    fix: remove ZenodoDownloader url argument in __init__

--- src/imgnet/download/downloaders.py
+++ src/imgnet/download/downloaders.py
@@ -100,7 +100,7 @@ class HuggingFaceDownloader(BaseDownloader):
 
 
 class ZenodoDownloader(BaseDownloader):
-    def __init__(self, record_id: str, url: str) -> None:
+    def __init__(self, record_id: str) -> None:
         self.record_id = record_id
         self.url = "https://zenodo.org/api/records"
 
